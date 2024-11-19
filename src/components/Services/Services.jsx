357f62722b7205744d9e70bc27c48674ba9a9221
abinjos307@gmail.com <abinjos307@gmail.com>
2024-11-20 00:50:11 +0530

    Service section content added

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -7,11 +7,15 @@ import develop from '../../assets/develop.png';
 export default function Services() {
     const handleMouseEnter = (e) => {
         const logo = e.currentTarget.querySelector('.service-item-logo');
-        logo.style.backgroundColor = 'white'; 
-        logo.style.border = "1px solid black"
+        logo.style.backgroundColor = '#7a7c7c'; 
+        logo.style.border = "1px solid #fff"
+        const desc = e.currentTarget.querySelector('.service-item-desc');
+        desc.style.color = 'black';
     }
     
     const handleMouseLeave = (e) => {
+        const desc = e.currentTarget.querySelector('.service-item-desc');
+        desc.style.color = 'white';
         const logo = e.currentTarget.querySelector('.service-item-logo');
         logo.style.backgroundColor = ''; 
         logo.style.border = ""
@@ -31,12 +35,24 @@ export default function Services() {
                     <div className="d-flex justify-content-between align-items-center row my-sm-3 my-lg-0 service-items">
                         <div className="service-item col-lg-4 col-sm-12" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
                             <div className="service-item-logo"><img src={uiux} alt="ui/ux" /></div>
+                            <div className="service-item-desc">
+                                <h3 className='pt-4 pb-1 service-item-head'>UI/ UX Designing</h3>
+                                <p className="py-2 service-item-para">UI/ UX in app is key to creating a great user experience. This will increase user loyality, strengthen the brand image, and provide a competitive advantage.</p>
+                            </div>
                         </div>
                         <div className="service-item col-lg-4 col-sm-12" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
                             <div className="service-item-logo"><img src={brand} alt="" /></div>
+                            <div className="service-item-desc">
+                                <h3 className='pt-4 pb-1 service-item-head'>Branding</h3>
+                                <p className="py-2 service-item-para">Branding can involves stratergies and efforts to build a consistent image, identity, and perception associated with a product, sevice, or company</p>
+                            </div>
                         </div>
                         <div className="service-item col-lg-4 col-sm-12" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
                             <div className="service-item-logo"><img src={develop} alt="" /></div>
+                            <div className="service-item-desc">
+                                <h3 className='pt-4 pb-1 service-item-head'>Developer</h3>
+                                <p className="py-2 service-item-para">Developers play an important role in creating technology solutions that make life easier, improve business processes, and drive innovations</p>
+                            </div>
                         </div>
                     </div>
                 </div>
