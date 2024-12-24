008182ee3af7663823da424bc747b45bdd6c48ab
abinjos307@gmail.com <abinjos307@gmail.com>
2024-12-24 21:21:36 +0530

    react btis tried

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,28 +1,56 @@
 import React from "react";
 import "./Hero.css";
 import group from '../../assets/group.jpg'
+import { BlurText } from "../Testimonials/BlurText";
+import CountUp from "../Dependencies/CountUp";
 
 export default function Hero() {
     return (
         <div className="col-11 mx-auto">
             <div className="row col-12 pt-5">
                 <div className="col-lg-7 col-sm-12">
-                    <h1 className="hero-head">
+                    {/* <h1 className="hero-head">
                         Color Digitally With Imagination Palette
-                    </h1>
+                    </h1> */}
+                    <BlurText text="Color Digitally With Imagination Palette" className="hero-head" delay={50} />
+
                 </div>
                 <div className="col-lg-5 col-sm-12 d-flex justify-content-center align-items-center">
-                    <p className="hero-para">
+                    <BlurText text="We are digital magicians, turning ideas into compelling designs, driving change and creating real impact." className="hero-para" delay={50} />
+                    {/* <p className="hero-para">
                         We are digital magicians, turning ideas into compelling designs,
                         driving change and creating real impact.
-                    </p>
+                    </p> */}
                 </div>
             </div>
             <div className="row col-12 mx-auto ">
                 <div className="col-lg-2 col-sm-12 d-flex flex-column justify-content-center gap-2">
-                    <div className="hero-sub-div my-1 py-2 px-4"><h2>5<span>+</span></h2><h6>Happy Clients</h6></div>
-                    <div className="hero-sub-div my-1 py-2 px-4"><h2>5<span>+</span></h2><h6>Happy Projects</h6></div>
-                    <div className="hero-sub-div my-1 py-2 px-4"><h2>5<span>+</span></h2><h6>Happy Faces</h6></div>
+                    <div className="hero-sub-div my-1 py-2 px-4"><h2><CountUp
+                        from={0}
+                        to={50}
+                        separator=","
+                        direction="up"
+                        duration={1}
+                        className="count-up-text"
+                    /><span className="plus">+</span></h2><h6>Happy Clients</h6></div>
+                    <div className="hero-sub-div my-1 py-2 px-4"><h2><CountUp
+                        from={0}
+                        to={50}
+                        separator=","
+                        direction="up"
+                        duration={1}
+                        className="count-up-text"
+                    /><span className="plus">+</span></h2><h6>Happy Projects</h6></div>
+                    <div className="hero-sub-div my-1 py-2 px-4"><h2><CountUp
+                        from={0}
+                        to={50}
+                        separator=","
+                        direction="up"
+                        duration={1}
+                        className="count-up-text"
+                    /><span className="plus">+</span></h2><h6>Happy Faces</h6></div>
+
+
                 </div>
                 <div className="col-lg-10 col-sm-12 d-flex justify-content-center align-items-center" style={{ position: "relative" }}>
                     {/* <svg viewBox="0 0 300 300">
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,4 +1,5 @@
 import { ListGroup } from 'react-bootstrap';
+import { BlurText } from './BlurText';
 import './Testimonials.css'
 import { h1 } from 'framer-motion/client';
 
@@ -26,7 +27,7 @@ export default function Testimonials() {
             <div className="col-12 d-flex align-items-center">
                 <a href="#about" className='howwework-btn px-3 mx-auto'>Testimonials</a>
             </div>
-            <h1 className="testimonial-head text-center mx-auto py-3 col-lg-8 col-sm-10">Hear From Our Clients</h1>
+            {/* <h1 className="testimonial-head text-center mx-auto py-3 col-lg-8 col-sm-10">Hear From Our Clients</h1> */}
             <p className='howwework-para text-center mx-auto py-2 col-lg-8 col-sm-10'>
                 This testimonial is a motivation for us to continue to improve quality and provide better satisfaction to every customer who entrusts us.
             </p>
